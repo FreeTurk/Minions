08ff770d2cc2c4eace9a9f89fe55809701baa826
FreeTurk <emiro@DESKTOP-VGKAMT9>
2021-02-08 19:41:30 +0300

    Dirt Update v1 WIP

---
+++ src/main/java/freeturk/minions/block/CompressedDirtBlock.java
@@ -0,0 +1,51 @@
+
+package freeturk.minions.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import java.util.List;
+import java.util.Collections;
+
+import freeturk.minions.MinionsModElements;
+
+@MinionsModElements.ModElement.Tag
+public class CompressedDirtBlock extends MinionsModElements.ModElement {
+	@ObjectHolder("minions:compressed_dirt")
+	public static final Block block = null;
+	public CompressedDirtBlock(MinionsModElements instance) {
+		super(instance, 56);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.EARTH).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0)
+					.harvestLevel(-1).harvestTool(ToolType.SHOVEL));
+			setRegistryName("compressed_dirt");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
